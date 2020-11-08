8115d4382cd4200c8c5657f5fa64063fb416a6c0
Алексей Стыров <tctfais@gmail.com>
2020-11-08 19:20:28 +0300

    Do Sega games

--- Pandorka/Pandorka.Designer.cs
+++ Pandorka/Pandorka.Designer.cs
@@ -32,7 +32,9 @@
             this.FolderWithGamesTextBox = new System.Windows.Forms.TextBox();
             this.DestinationFolderLabel = new System.Windows.Forms.Label();
             this.DestinationFolderTextBox = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.DoButton = new System.Windows.Forms.Button();
+            this.GameTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.ChooseGameTypeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FolderWithGamesLabel
@@ -50,6 +52,7 @@
             this.FolderWithGamesTextBox.Name = "FolderWithGamesTextBox";
             this.FolderWithGamesTextBox.Size = new System.Drawing.Size(535, 20);
             this.FolderWithGamesTextBox.TabIndex = 1;
+            this.FolderWithGamesTextBox.Text = "D:\\Games\\!SegaNes\\112GAME";
             this.FolderWithGamesTextBox.Click += new System.EventHandler(this.FolderWithGamesTextBox_Click);
             // 
             // DestinationFolderLabel
@@ -67,19 +70,43 @@
             this.DestinationFolderTextBox.Name = "DestinationFolderTextBox";
             this.DestinationFolderTextBox.Size = new System.Drawing.Size(535, 20);
             this.DestinationFolderTextBox.TabIndex = 3;
+            this.DestinationFolderTextBox.Text = "D:\\Games\\!SegaNes";
             this.DestinationFolderTextBox.Click += new System.EventHandler(this.DestinationFolderTextBox_Click);
             // 
-            // button1
+            // DoButton
             // 
-            this.button1.AutoSize = true;
-            this.button1.Location = new System.Drawing.Point(159, 245);
-            this.button1.Name = "button1";
-            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.button1.Size = new System.Drawing.Size(233, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Do";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.DoButton.AutoSize = true;
+            this.DoButton.Location = new System.Drawing.Point(168, 226);
+            this.DoButton.Name = "DoButton";
+            this.DoButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.DoButton.Size = new System.Drawing.Size(233, 40);
+            this.DoButton.TabIndex = 4;
+            this.DoButton.Text = "Do";
+            this.DoButton.UseVisualStyleBackColor = true;
+            this.DoButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GameTypeComboBox
+            // 
+            this.GameTypeComboBox.FormattingEnabled = true;
+            this.GameTypeComboBox.Items.AddRange(new object[] {
+            "*.zip Final Burn Alpha 0.2.97.42 Modified by Moorechip (fba42) \"FBA_SDL_main\",\"FB" +
+                "A_compatible\" (0 type)",
+            "*.chd, *.cdi, *.iso, *.elf, *.cue, *.gdi, *.lst, *.bin, *.dat, *.zip, *.7z  libre" +
+                "tro_reicast (DC) retro_arch_main\r (18 type)"});
+            this.GameTypeComboBox.Location = new System.Drawing.Point(15, 110);
+            this.GameTypeComboBox.MaxDropDownItems = 20;
+            this.GameTypeComboBox.Name = "GameTypeComboBox";
+            this.GameTypeComboBox.Size = new System.Drawing.Size(535, 21);
+            this.GameTypeComboBox.TabIndex = 5;
+            // 
+            // ChooseGameTypeLabel
+            // 
+            this.ChooseGameTypeLabel.AutoSize = true;
+            this.ChooseGameTypeLabel.Location = new System.Drawing.Point(12, 91);
+            this.ChooseGameTypeLabel.Name = "ChooseGameTypeLabel";
+            this.ChooseGameTypeLabel.Size = new System.Drawing.Size(103, 13);
+            this.ChooseGameTypeLabel.TabIndex = 6;
+            this.ChooseGameTypeLabel.Text = "Choose games type:";
             // 
             // Pandorka
             // 
@@ -87,12 +114,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(569, 280);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(566, 278);
+            this.Controls.Add(this.ChooseGameTypeLabel);
+            this.Controls.Add(this.GameTypeComboBox);
+            this.Controls.Add(this.DoButton);
             this.Controls.Add(this.DestinationFolderTextBox);
             this.Controls.Add(this.DestinationFolderLabel);
             this.Controls.Add(this.FolderWithGamesTextBox);
             this.Controls.Add(this.FolderWithGamesLabel);
+            this.MaximumSize = new System.Drawing.Size(582, 317);
+            this.MinimumSize = new System.Drawing.Size(582, 317);
             this.Name = "Pandorka";
             this.Text = "Pandorka";
             this.ResumeLayout(false);
@@ -106,7 +137,9 @@
         private System.Windows.Forms.TextBox FolderWithGamesTextBox;
         private System.Windows.Forms.Label DestinationFolderLabel;
         private System.Windows.Forms.TextBox DestinationFolderTextBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DoButton;
+        private System.Windows.Forms.ComboBox GameTypeComboBox;
+        private System.Windows.Forms.Label ChooseGameTypeLabel;
     }
 }
 
--- Pandorka/Pandorka.cs
+++ Pandorka/Pandorka.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,27 @@ namespace Pandorka
 {
     public partial class Pandorka : Form
     {
+        readonly string Template = @"<game emulator={type}>
+<information>
+<description>{description}</description>
+<genre>1</genre>
+</information>
+<file>
+<rom name ={romName}/>
+</file>
+<translation>
+<string language={language}>
+<name>{gameName}</name>
+</string>
+</translation>
+<config>
+<load_time>0</load_time>
+<free_play>1</free_play>
+<save_state>1</save_state>
+<start_directly>1</start_directly>
+</config>
+</game>";
+
         public Pandorka()
         {
             InitializeComponent();
@@ -37,11 +59,49 @@ namespace Pandorka
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(FolderWithGamesTextBox.Text) || string.IsNullOrEmpty(DestinationFolderTextBox.Text)) {
-                MessageBox.Show("Choose all path!");
+            DoButton.Enabled = false;
+            if (string.IsNullOrEmpty(FolderWithGamesTextBox.Text) || string.IsNullOrEmpty(DestinationFolderTextBox.Text)
+                    || string.IsNullOrEmpty(GameTypeComboBox.Text)) {
+                MessageBox.Show("Choose all path!", "Error application execute!");
+                DoButton.Enabled = true;
                 return;
             }
 
+            string masterGameFolder = DestinationFolderTextBox.Text + @"\mcgames";
+            
+            Directory.CreateDirectory(masterGameFolder);
+
+            var folderWithGames = new DirectoryInfo(FolderWithGamesTextBox.Text);
+
+            foreach (FileInfo gameFile in folderWithGames.GetFiles())
+            {
+                var templateXml = Template;
+                string fileNameClear = Path.GetFileNameWithoutExtension(gameFile.Name.Replace(" ", ""));
+                string fileName = Path.GetFileNameWithoutExtension(gameFile.Name);
+                string pathGame = masterGameFolder + @"\" + fileNameClear;
+                string fileGameFull = pathGame + @"\" + fileNameClear;
+                Directory.CreateDirectory(pathGame);
+                File.Copy(gameFile.FullName, pathGame + @"\" + gameFile.Name.Replace(" ", ""), true);
+                templateXml = templateXml.Replace("{description}", "\"" + fileName + "\"");
+                templateXml = templateXml.Replace("{romName}", "\"" + gameFile.Name.Replace(" ","") + "\"");
+                templateXml = templateXml.Replace("{type}", "\"15\"");
+                templateXml = templateXml.Replace("{language}", "\"en\"");
+                templateXml = templateXml.Replace("{gameName}", "\"" + fileName + "\"");
+                
+
+                using (StreamWriter swXml = new StreamWriter(fileGameFull + ".xml", false, System.Text.Encoding.Default))
+                {
+                    swXml.WriteLine(templateXml);
+                }
+                
+                using (StreamWriter swXml = new StreamWriter(masterGameFolder + @"\" + "install.txt", true, System.Text.Encoding.Default))
+                {
+                    swXml.WriteLine(fileNameClear);
+                }
+
+            }
+            MessageBox.Show("Finish! And now you must convert usb-flash to exFat (32kb) and copy msgames directory!", "Done!");
+            DoButton.Enabled = true;
         }
     }
 }
